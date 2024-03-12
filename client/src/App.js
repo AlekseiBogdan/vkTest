6a75b1a455860a6cc693a69812f84543f38e8128
AlekseiBogdan <apbkgdan@gmail.com>
2024-03-13 00:18:13 +0300

    feat: refractor components for Suspense fallback to work fix: fix group.closed typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,62 +1,6 @@
-import React, { Suspense, useState } from 'react'
+import React, { lazy, Suspense } from 'react'
 
-async function GetGroupResponse() {
-  const resp = await fetch('/api')
-  const data = await resp.json()
-  return data
-}
-
-const groups = await GetGroupResponse()
-
-const groupStyle = {
-  width: '2000px',
-  display: 'flex',
-  gap: '100px',
-  boxSizing: 'border-box',
-  margin: '50px 20px'
-}
-
-const groupInfoStyle = {
-  display: 'flex',
-  flexDirection: 'column'
-}
-
-const ReturnGroups = () => {
-  return (<div>
-    {
-      groups.map((group, i) => (
-        <OneGroup key={`group-${group.id}`} group={group}/>
-      ))
-    }
-        </div>)
-}
-
-const OneGroup = ({group}) => {
-  const [friends, setFriends] = useState(false)
-
-  const handleClick = () => {
-    setFriends(friends => !friends)
-  }
-  return (
-        <div style={groupStyle}>
-          <div style={{width: '100px', height: '100px', borderRadius: '50%', background: group.avatar_color}}></div>
-          <div style={groupInfoStyle}>
-            <h2>{group.name}</h2>
-            <p>Тип приватности: {group.closed ? 'открытая' : 'закрытая'}</p>
-            <p>Количество подписчиков: {group.members_count}</p>
-            <p style={{cursor: 'pointer'}} onClick={handleClick}>Количество друзей: {group.friends ? group.friends.length : 0}</p>
-            
-              {(() => {
-                if (friends && group.friends) {
-                  return (<p>{Object.values(group.friends).map((friend, i) => {
-                    return (<span key={`${group.id}-friend-${i+1}`}>{`${friend.first_name} ${friend.last_name}; `}</span>)
-                  })}</p>)
-                }
-              })()}
-          </div>
-        </div>
-  )
-}
+const ReturnGroups = lazy(() => import('./ReturnGroups'))
 
 export function App(props) {
   return (
---
+++ client/src/ReturnGroups.js
@@ -0,0 +1,62 @@
+﻿import {useState} from 'react'
+
+async function GetGroupResponse() {
+    const resp = await fetch('/api')
+    const data = await resp.json()
+    return data
+  }
+  
+  const groups = await GetGroupResponse()
+  
+  const groupStyle = {
+    width: '2000px',
+    display: 'flex',
+    gap: '100px',
+    boxSizing: 'border-box',
+    margin: '50px 20px'
+  }
+  
+  const groupInfoStyle = {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+  
+  const OneGroup = ({group}) => {
+    const [friends, setFriends] = useState(false)
+  
+    const handleClick = () => {
+      setFriends(friends => !friends)
+    }
+    return (
+          <div style={groupStyle}>
+            <div style={{width: '100px', height: '100px', borderRadius: '50%', background: group.avatar_color}}></div>
+            <div style={groupInfoStyle}>
+              <h2>{group.name}</h2>
+              <p>Тип приватности: {group.closed ? 'закрытая' : 'открытая'}</p>
+              <p>Количество подписчиков: {group.members_count}</p>
+              <p style={{cursor: 'pointer'}} onClick={handleClick}>Количество друзей: {group.friends ? group.friends.length : 0}</p>
+              
+                {(() => {
+                  if (friends && group.friends) {
+                    return (<p>{Object.values(group.friends).map((friend, i) => {
+                      return (<span key={`${group.id}-friend-${i+1}`}>{`${friend.first_name} ${friend.last_name}; `}</span>)
+                    })}</p>)
+                  }
+                })()}
+            </div>
+          </div>
+    )
+  }
+
+  const ReturnGroups = () => {
+    return (<div>
+      {
+        groups.map((group, i) => (
+          <OneGroup key={`group-${group.id}`} group={group}/>
+        ))
+      }
+          </div>)
+  }
+
+  export default ReturnGroups
+  
